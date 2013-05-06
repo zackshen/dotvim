0ed238cf09988b6cbb483881181451163ba7f0f0
zack <zackshen0526@gmail.com>
2013-05-06 11:10:34 +0800

    add vim jquery syntax highlight

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ git submodule add git://github.com/tpope/vim-surround.git bundle/vim-surround
 git submodule add git://github.com/pangloss/vim-javascript.git bundle/vim-javascript
 git submodule add git://github.com/flazz/vim-colorschemes.git bundle/vim-colorschemes
 git submodule add git://github.com/Lokaltog/vim-powerline.git bundle/vim-powerline
+git submodule add git://github.com/nono/jquery.vim.git bundle/vim-jquery
 
 git submodule init
 git submodule update
