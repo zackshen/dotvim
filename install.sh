b5280b3817eaef9500695689d1af0d89c5e11163
zack <zackshen0526@gmail.com>
2015-06-06 21:50:00 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,21 +4,26 @@
 #
 #########################################
 
+install_vim() {
+    VIM_RT=`pwd`
+    if [ ! -d $VIM_RT/bundle ]; then
+        mkdir -p $VIM_RT/bundle
+    fi
 
-VIM_RT=`pwd`
-if [ ! -d $VIM_RT/bundle ]; then
-    mkdir -p $VIM_RT/bundle
-fi
+    if [ ! -e $VIM_RT/bundle/vundle ]; then
+        echo "Installing Vundle"
+        git clone https://github.com/gmarik/vundle.git $VIM_RT/bundle/vundle
+    else
+        exit 1
+    fi
 
-if [ ! -e $VIM_RT/bundle/vundle ]; then
-    echo "Installing Vundle"
-    git clone https://github.com/gmarik/vundle.git $VIM_RT/bundle/vundle
-fi
+    rm -rf ~/.vimrc
+    rm -rf ~/.vim
 
-rm -rf ~/.vimrc
-rm -rf ~/.vim
+    ln -s $VIM_RT/vimrc ~/.vimrc
+    ln -s $VIM_RT ~/.vim
 
-ln -s $VIM_RT/vimrc ~/.vimrc
-ln -s $VIM_RT ~/.vim
+    vim -u ~/.vim/vim.bundles +BundleInstall +BundleClean +qall
+}
 
-vim -u ~/.vim/vim.bundles +BundleInstall +BundleClean +qall
+install_vim
